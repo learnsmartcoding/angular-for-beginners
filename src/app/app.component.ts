15df6ae778e4cfa9c20ab313374790235ed97cc9
karthiktechblog <56040962+karthiktechblog@users.noreply.github.com>
2022-02-22 14:35:37 -0500

    Animating Route Transitions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { slideInAnimation } from './app.animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [slideInAnimation]
 })
 export class AppComponent {
   title = 'LSC-Restaurant';
